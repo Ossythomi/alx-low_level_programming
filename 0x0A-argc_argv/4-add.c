ed572b196a592df2d233362685ddd5313dbd661e
Ossythomi <ossythomi@yahoo.com>
2022-11-04 06:34:59 -0700

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,32 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "main.h"
 
 /**
- * main - Program that takes first two integer arguments and prints the product
+ * main - Program that takes in all integer arguments and returns the sum
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: 1 if not enough arguments passed in, 0 otherwise
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
  */
 
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1 || argc == 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+	if (argc < 2)
+	printf("0\n");
 	else
 	{
-		j = 1;
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
 
-		for (i = 1; i < 3; i++)
-		j *= atoi(argv[i]);
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
 
-		printf("%d\n", j);
+			sum += atoi(argv[i]);
+		}
+
+		printf("%d\n", sum);
 	}
-
 	return (0);
 }
